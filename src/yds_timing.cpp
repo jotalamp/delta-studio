9e63361ed239ef522485bc63cb43b98dacab74de
bobsayshilol <bobsayshilol@live.co.uk>
2022-08-14 00:01:48 +0100

    GCC fixes

--- src/yds_timing.cpp
+++ src/yds_timing.cpp
@@ -89,7 +89,9 @@ void ysTimingSystem::Update() {
 }
 
 void ysTimingSystem::Initialize() {
+#if _MSC_VER
     qpcFlag = (QueryPerformanceFrequency(&qpcFrequency) > 0);
+#endif
 
     m_frameNumber = 0;
 
